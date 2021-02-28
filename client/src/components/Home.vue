5c930d9e5bdc2cc3cb7d81dd5dd9f256b6ec469a
csaba <szabo.csaba96@outlook.com>
2021-02-28 09:08:33 +0100

    now all the pictures appear

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <b-grid>
-            <b-row>
-                <b-col>
-                    <img v-for="fileName in fileNames" :key="fileName.id" :src="getImageFromAPI + fileName" alt="picture"/>
+            <b-row >
+                <b-col class="imageRow">
+                    <img class="image" v-for="fileName in fileNames" :key="fileName.id" :src="getImageFromAPI + fileName" alt="picture"/>
                 </b-col>
             </b-row>
             <b-row>
@@ -44,5 +44,18 @@ export default {
 }
 </script>
 <style scoped>
-
+    .image {
+        display: block;
+        max-width: 300px;
+        max-height: 300px;
+        min-width: 300px;
+        min-height: 300px;
+        margin-right: 20px;
+        margin-left: 30px;
+        box-shadow: 10px 10px 5px grey;
+        float: left;
+    }
+    .imageRow {
+        content: center;
+    }
 </style>
